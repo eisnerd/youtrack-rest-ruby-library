c48e67e8376a8af9bbb4d92d8423186d9cf0b2ff
Jason Rickman <eta503@gmail.com>
2012-01-12 15:36:48 -0600

    - Moved Issue and Project to their own files

--- lib/youtrack_api/issue.rb
+++ lib/youtrack_api/issue.rb
@@ -92,36 +92,5 @@ module YouTrackAPI
 
   end
 
-  class Project
-
-    attr_accessor :lead, :name, :id
-
-    def initialize(connection, project_name, project_id = nil, owner = nil)
-      @conn = connection
-      @id = project_id
-      @name = project_name
-      @lead = owner
-    end
-
-    def get
-      project = REXML::XPath.first(REXML::Document.new(@conn.request(:get, self.path).body), "//project")
-      [:name, :id, :lead].each{|elem| instance_variable_set("@#{elem}", project.attributes[elem.to_s])}
-    end
-
-    def put
-
-    end
-
-    def post
-
-    end
-
-    private
-
-    def path
-      "#{@conn.rest_path}/admin/project/#{id}"
-    end
-
-  end
-  
 end
+      
